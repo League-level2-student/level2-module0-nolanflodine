da71fa9a8aebc2a66db027f041a7305bbe287c95
23nfl <23nfl@DESKTOP-DGNSB50.attlocal.net>
2019-10-12 16:58:02 -0700

    array

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,16 +1,47 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+public static void main(String[] args) {
+	
 	//2. create an array of 5 robots.
-
+Robot [] robot= new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+for (int i = 0; i < robot.length; i++) {
+robot[i]=new Robot();
+System.out.println(i);
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+robot[0].setY(500);
+robot[0].setX(100);
+robot[1].setY(500);
+robot[1].setX(200);
+robot[2].setY(500);
+robot[2].setX(300);
+robot[3].setY(500);
+robot[3].setX(400);
+robot[4].setY(500);
+robot[4].setX(500);
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
+
+for (int i = 0; i < robot.length; i++) {
+	if (robot[i].getY()>=0) {
+	robot[i].setSpeed(10);
+	Random r = new Random();
+	int rand=r.nextInt(50);
+	robot[i].move(rand);
+	}
+	else {
+		robot[i].setSpeed(10);
+		robot[i].turn(1440);
+	}
+}
+}
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
@@ -19,4 +50,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
